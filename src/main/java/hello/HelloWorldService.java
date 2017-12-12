49598d602bee5edd7d9021c5c30ff18cbd619ca7
andyeglim <andyeglim@gmail.com>
2017-12-13 10:58:03 +1300

    HelloWorld

--- src/main/java/hello/HelloWorldService.java
+++ src/main/java/hello/HelloWorldService.java
@@ -1,13 +1,13 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.HtmlUtils;
 
 @Component
 public class HelloWorldService {
 
     public String getHelloMessage(final String name) {
-        return "Hello " + name;
+        return "Hello " + HtmlUtils.htmlEscape(name);
     }
 
 }
